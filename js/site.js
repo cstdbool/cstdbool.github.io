fc67dd257d1ebe8dee3798731c553728edbebacc
Antonio Junaković <cstdbool@gmail.com>
2021-02-10 19:51:09 +0100

    Add contributions page

--- js/site.js
+++ js/site.js
@@ -9,27 +9,32 @@ function openPage(address) {
 	});
 }
 
+function completeLoad() {
+	loadInProgress = false;
+}
+
 function loadPage(address) {
 	let displayAddress = address;
 	while (displayAddress.startsWith('/'))
 		displayAddress = displayAddress.substring(1);
 	if (displayAddress.toLowerCase().endsWith('.html'))
 		displayAddress = displayAddress.substring(0, displayAddress.length - 5);
-	if (displayAddress.toLowerCase() === 'home')
+	if (displayAddress.toLowerCase() == 'home')
 		window.history.replaceState('', '', '/');
 	else
 		window.history.replaceState('', '', '/?view=' + encodeURI(displayAddress));
 	$('#section-content').html('');
 	$('#section-content').css({opacity: 0.0});
 	$('#section-content').load(address, function(responseText, textStatus) {
-		if ((textStatus === 'success') || (textStatus === 'notmodified')) {
+		if ((textStatus == 'success') || (textStatus == 'notmodified')) {
 			$('#section-content').animate({opacity: 1.0}, 300, function() {
-				loadInProgress = false;
+				if ($('#manual-complete-load').length == 0)
+					completeLoad();
 			});
 		} else {
 			$('#section-content').html('<h1>Page not found</h1>\n<p>The page you were looking for does not exist.</p>');
 			$('#section-content').animate({opacity: 1.0}, 300, function() {
-				loadInProgress = false;
+				completeLoad();
 			});
 		}
 	});
